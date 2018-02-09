60b13e4a9f1dd808105f746d9eec7df11adda04b
Przemysław Chmielewski <p.chmielewski91@o2.pl>
2018-02-09 14:28:52 +0100

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LottoDraw
+The application was created in November 2017. I used the srand() function. The application has error - can repeat numbers.
+
+I used IDE:
+- CodeBlocks
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int liczba;
+int main()
+{
+    cout << "Witaj w loswaniu! Za 3 sekundy nastapi zwolnienie blokady"<<endl;
+    Sleep(3000);
+    srand(time(NULL));
+    for (int i=1; i<=6; i++)
+    {
+    liczba = rand()%49+1;
+    Sleep(1000);
+    cout<<liczba<<"\a"<<endl;
+
+    }
+
+
+    return 0;
+}
